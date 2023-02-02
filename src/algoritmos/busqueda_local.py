9747122289367c0b40973a4c83aa8c6daa5f11bf
DIR\sofia.chazarra <sofia.chazarra@accenture.com>
2023-02-02 11:17:03 +0100

    busqueda_local y greedy

--- src/algoritmos/busqueda_local.py
+++ src/algoritmos/busqueda_local.py
@@ -1,4 +1,8 @@
+import time
+import src.auxiliares.read_data as aux
+
 def busqueda_local(data, M):
+    start=time.time()
     peso = data["weight"].tolist()
 
     beneficio = data["imp"].tolist()
@@ -19,4 +23,20 @@ def busqueda_local(data, M):
         else:
             solucion[i] = round((M - peso_actual) / peso[i])
             peso_actual = M
+        end = time.time()
+
+    print("Usando un algoritmo Greedy para el problema de la mochila"
+         " \n" + " Solución: " + str(solucion) + "\n Número de elementos: "
+         + str(sum(solucion)) + "\n Peso: " + str(peso_actual) + "\n"
+        " Tiempo de cómputo = " + str(1000 * (end - start)) + " ms")
     return solucion, peso_m
+
+    ###Para ejecutarlo directamente
+
+path_data = "C:/Users/sofia.chazarra/OneDrive - Accenture/Documents/MasterUCM/OptimizacionII/DatosMochila/kplib-master/"
+file="00Uncorrelated/n00050/R01000/s001.kp"
+path_file=path_data+file
+
+peso_tot,num_obj,data=aux.read_data(path_file)
+data,sort_data=aux.prop_data(num_obj,data)
+busqueda_local(sort_data,peso_tot)
