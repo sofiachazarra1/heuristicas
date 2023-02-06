a2f2fb7a3b9abaf0bca3943dba260b5e06a5cd96
DIR\sofia.chazarra <sofia.chazarra@accenture.com>
2023-02-06 15:52:43 +0100

    .

--- src/algoritmos/vns.py
+++ src/algoritmos/vns.py
@@ -10,7 +10,6 @@ n_max = 1000
 def C(data):
     """Función de coste asociada al problema de la mochila."""
     suma_sol=0
-    print(data)
     for s in data:
         suma_sol += int(s)
     return -(suma_sol)
@@ -19,17 +18,15 @@ def C(data):
 def VND(sort_data, peso_tot, data, num_obj):
     """Búsqueda Descendente"""
 
-    S, agg_weight = greedy_mochila(sort_data, peso_tot, sort_data, False)
+    S, agg_weight = greedy_mochila(sort_data, peso_tot, data, False)
     j=0
     while (j <= 2):
-        S_prima = busquedalocal(sort_data, peso_tot, sort_data, num_obj, False, j)
+        S_prima = busquedalocal(sort_data, peso_tot, data, num_obj, False, j)
         if C(S_prima) < C(S):
             S = S_prima
             j = 0
         else:
             j = j + 1
-
-
     return S
 
 
@@ -44,23 +41,24 @@ def GVNS(sort_data, peso_tot, data, num_obj):
     entorno = int(random.choice([0, 1, 2]))
     S_prima = busquedalocal(sort_data, peso_tot, data, num_obj, False, entorno)
 
-    S_segunda = VND(sort_data, peso_tot, S_prima, num_obj)
-    print("S_sgunda:"+str(S_segunda))
-    print("S:"+str(S))
-    print(S[0])
-    while (n < n_max and C(S_segunda) < C(S[0])):
+    S_segunda = VND(sort_data, peso_tot, data, num_obj)
+    if type(S[0])==list:
+        T=S[0]
+    else:
+        T=S
+    while (n < n_max and C(S_segunda) < C(T)):
         j = 1
         while (j <= 2):
             S = S_segunda
             entorno = int(random.choice([0, 1, 2]))
             S_prima = busquedalocal(sort_data, peso_tot, data, num_obj, False, entorno)
-            S_segunda = VND(sort_data, peso_tot, S_prima, num_obj)
+            S_segunda = VND(sort_data, peso_tot, data, num_obj)
             j = j + 1
         n = n + 1
     end = time.time()
     print("Usando un algoritmo vns para el problema de la mochila"
           " \n" + " Solución: " + str(S) + "\n Número de elementos: "
-          + str(sum(S)) + "\n Peso: " + str(aux.calcular_peso(data["weight"], S,num_obj)) + "\n"
+          + str(sum(S)) + "\n Peso: " + str(aux.calcular_peso(data["weight"], S, num_obj)) + "\n"
         " Tiempo de cómputo = " + str(1000 * (end - start)) + " ms")
 
     return S
